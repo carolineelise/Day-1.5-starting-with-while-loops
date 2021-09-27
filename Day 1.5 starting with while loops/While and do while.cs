58638c1fc030673ee33c7b68e82203a2bbef210f
Caroline Wilson <carolineelise616@gmail.com>
2021-09-27 10:23:49 -0400

    Add project files.

---
+++ Day 1.5 starting with while loops/While and do while.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Day_1._5_starting_with_while_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am so lost, but I will work hard to catch up :)");
+                                            //WHILE
+            //  var idx = 1;
+            //while (idx <= 25)
+            //{
+            //    Console.Write($"{idx * idx * idx}, ");
+              //  idx = idx + 2;
+            //} 
+                                            //DO WHILE
+           //{
+              //  var idx = 1; do
+                //{
+                  //  Console.Write($"{idx * idx * idx}, ");
+                    //idx = idx + 2;
+                //}
+               // while (idx <= 25);
+               
+
+          // }
+
+
+        }
+    }    
+}
